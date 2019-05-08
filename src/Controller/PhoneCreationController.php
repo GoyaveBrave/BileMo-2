1432ecce268e05b3572f0280b73cb6c32b92d287
RomainDW <romain.ollier34@gmail.com>
2019-05-08 11:29:59 +0200

    Creation of a simple PhoneCreationController with a createPhone action

---
+++ src/Controller/PhoneCreationController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Phone;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class PhoneCreationController extends AbstractController
+{
+    /**
+     * @Route("/phone/creation", name="phone_creation")
+     *
+     * @param Request             $request
+     * @param SerializerInterface $serializer
+     *
+     * @return Response
+     */
+    public function CreatePhone(Request $request, SerializerInterface $serializer)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $data = $request->getContent();
+        $phone = $serializer->deserialize($data, Phone::class, 'json');
+
+        // TODO: Verification
+        $em->persist($phone);
+        $em->flush();
+
+        return new Response('', Response::HTTP_CREATED);
+    }
+}
