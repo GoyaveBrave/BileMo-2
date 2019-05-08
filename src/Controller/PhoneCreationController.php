d0e852223cd968bec24bbd2de5f7cd8cf9a42fa6
RomainDW <romain.ollier34@gmail.com>
2019-05-08 13:45:11 +0200

    Add API Documentation in all existing Controllers

--- src/Controller/PhoneCreationController.php
+++ src/Controller/PhoneCreationController.php
@@ -3,16 +3,35 @@
 namespace App\Controller;
 
 use App\Entity\Phone;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Swagger\Annotations as SWG;
 
 class PhoneCreationController extends AbstractController
 {
     /**
-     * @Route("/phone/creation", name="phone_creation")
+     * Create a phone entity.
+     *
+     * @Route("/phone/creation", name="phone_creation", methods={"POST"})
+     *
+     * @SWG\Response(
+     *     response=201,
+     *     description="The phone has been created",
+     * )
+     *
+     * @SWG\Parameter(
+     *     name="phone",
+     *     in="body",
+     *     description="The information of the phone",
+     *     type="json",
+     *     @SWG\Schema(ref=@Model(type=Phone::class))
+     * )
+     *
+     * @SWG\Tag(name="phones")
      *
      * @param Request             $request
      * @param SerializerInterface $serializer
--- src/Controller/PhoneShowController.php
+++ src/Controller/PhoneShowController.php
@@ -7,12 +7,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Swagger\Annotations as SWG;
 
 class PhoneShowController extends AbstractController
 {
     /**
-     * @Route("/phone/{id}", name="phone_show")
+     * Get a phone by ID.
+     *
+     * @Route("/phone/{id}", name="phone_show", methods={"GET"})
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="The requested phone has been found",
+     *     @Model(type=Phone::class)
+     * )
+     *
+     * @SWG\Tag(name="phones")
+     *
      * @param Phone $phone
+     *
      * @return JsonResponse
      */
     public function getPhone(Phone $phone)
