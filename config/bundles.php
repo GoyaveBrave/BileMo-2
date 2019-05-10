5cba26483726bb476a782bd87ec998e495bb1eae
RomainDW <romain.ollier34@gmail.com>
2019-05-10 20:16:39 +0200

    Install LexikJWTAuthenticationBundle + configuration

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
