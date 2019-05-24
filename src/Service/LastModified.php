76b4b6997b03aa267b178986d799f2300725b68f
RomainDW <romain.ollier34@gmail.com>
2019-05-24 13:43:12 +0200

    Update getLastModified function to work with the new OutputInterface

--- src/Service/LastModified.php
+++ src/Service/LastModified.php
@@ -2,17 +2,20 @@
 
 namespace App\Service;
 
+use App\Output\Interfaces\OutputInterface;
+use App\Output\Output;
 use DateTime;
 
 class LastModified
 {
-    public static function getLastModified(array $entities)
+    public static function getLastModified(OutputInterface $entities)
     {
         $mostRecent = 0;
 
-        foreach ($entities as $entity) {
-            if (method_exists($entity, 'getUpdatedAt')) {
-                $date = $entity->getUpdatedAt()->getTimestamp();
+        /** @var Output $entity */
+        foreach ($entities->getData() as $entity) {
+            if (isset($entity->getAttributes()['updated_at']) && $updatedDate = $entity->getAttributes()['updated_at']) {
+                $date = strtotime($updatedDate);
                 if ($date > $mostRecent) {
                     $mostRecent = $date;
                 }
